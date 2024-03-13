c986dc38e703197bf96f7b0335b3be6e02d27547
dn1v <83037558+dn1v@users.noreply.github.com>
2024-03-13 14:01:13 +0100

    working on network components

--- src/app/features/network/requests/request-form/request-form.component.ts
+++ src/app/features/network/requests/request-form/request-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-request-form',
   templateUrl: './request-form.component.html',
   styleUrls: ['./request-form.component.css']
 })
-export class RequestFormComponent {
+export class RequestFormComponent implements OnInit {
 
+    form: FormGroup = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email])
+    })
+
+    constructor() { }
+
+    ngOnInit() { }
+
+    onSendRequest(): void {
+        console.log(this.email?.value)
+    }
+
+    get email() {
+        return this.form.get('email')
+    }
 }
